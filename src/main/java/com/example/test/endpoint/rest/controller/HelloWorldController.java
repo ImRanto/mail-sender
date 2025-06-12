d85e108c34f59153fc58d758e6690bf9709007f2
Ranto <hei.ranto.2@gmail.com>
2025-06-12 21:31:01 +0300

    feat: add model and controller to send mail requested

--- src/main/java/com/example/test/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/com/example/test/endpoint/rest/controller/HelloWorldController.java
@@ -1,7 +1,11 @@
 package com.example.test.endpoint.rest.controller;
 
+import com.example.test.endpoint.event.EventProducer;
+import com.example.test.endpoint.event.model.SendEmailRequested;
 import com.example.test.service.HelloWorldService;
+import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class HelloWorldController {
   private final HelloWorldService service;
+  private final EventProducer<SendEmailRequested> eventProducer;
 
   @GetMapping("/hello")
   public String helloWorld(@RequestParam String name) {
     return service.uploadHelloWorldMessage(name);
+  }
+
+  @GetMapping("/hello")
+  @SneakyThrows
+  public String helloWorldMailer(@RequestParam String to) {
+    var event = SendEmailRequested.builder().to(to).build();
+    eventProducer.accept(List.of(event));
+    return "... world!";
   }
 }
