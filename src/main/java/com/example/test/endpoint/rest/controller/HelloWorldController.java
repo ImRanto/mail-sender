3fed72d33ef49c381ad0b6cb8251eabc718502e4
Ranto <hei.ranto.2@gmail.com>
2025-06-23 16:14:45 +0300

    fix: modify helloworldController, remove another /hello

--- src/main/java/com/example/test/endpoint/rest/controller/HelloWorldController.java
+++ src/main/java/com/example/test/endpoint/rest/controller/HelloWorldController.java
@@ -2,7 +2,6 @@ package com.example.test.endpoint.rest.controller;
 
 import com.example.test.endpoint.event.EventProducer;
 import com.example.test.endpoint.event.model.SendEmailRequested;
-import com.example.test.service.HelloWorldService;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.SneakyThrows;
@@ -13,17 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @AllArgsConstructor
 public class HelloWorldController {
-  private final HelloWorldService service;
   private final EventProducer<SendEmailRequested> eventProducer;
 
   @GetMapping("/hello")
-  public String helloWorld(@RequestParam String name) {
-    return service.uploadHelloWorldMessage(name);
-  }
-
-  @GetMapping("/hello")
   @SneakyThrows
-  public String helloWorldMailer(@RequestParam String to) {
+  public String helloWorld(@RequestParam String to) {
     var event = SendEmailRequested.builder().to(to).build();
     eventProducer.accept(List.of(event));
     return "... world!";
